cb701e92f51fad9f663147b3d5830ea7304fc5fc
Nick59dev <justApple400@gmail.com>
2023-02-18 17:40:34 +0700

    ver first commit (zigzag problem added)

---
+++ zigzag/zigzag.go
@@ -0,0 +1,35 @@
+package medium
+
+func convert(s string, numRows int) string {
+    if numRows <= 0{
+        return ""
+    }
+    if numRows == 1 {
+        return s
+    }
+
+    result := []rune{}
+    filter := make(map[int][]rune)
+
+    step  := -1
+    point := 1
+
+    for _, x := range s {
+        if point == 1 || point == numRows {
+            step *= -1
+        }
+
+        fmt.Println(point)
+
+        filter[point] = append(filter[point], rune(x))
+        point += step
+    }
+
+    for i := 1; i <= numRows; i++ {
+        for _, x := range filter[i] {
+            result = append(result, rune(x))
+        }
+    }
+
+    return string(result)
+}
